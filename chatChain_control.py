eb86f433dd728916a5c57681016676cf710204e3
aeternitas8 <43860393+aeternitas8@users.noreply.github.com>
2018-12-09 23:04:29 -0800

    code cleanup and commenting

--- chatChain_control.py
+++ chatChain_control.py
@@ -48,6 +48,7 @@ def do_nothing():
 
 def get_valid_int(validInts,prompt):
 	#begin get_valid_int
+	#get calid int from user
 	while True:
 		userValue=input(prompt)
 		try:
@@ -80,20 +81,20 @@ def send_message():
 
 def load_addressbook(userName):
 	#begin load_addressbook
-	global ADDRESSBOOK
+	global ADDRESSBOOK #set var to global scope
 
 	if DEBUG:print(userName);print(USER_NAME)
+
 	#get addressbook data from the blockchain
 	userAddressbookKey=ADDRESSBOOK_KEY_PREFIX+userName
 	addressbookData=BLOCK.get_message_blockchain(userAddressbookKey)
 
-	if DEBUG:print('addressbook:', len(addressbookData),addressbookData,line_number())
+	if DEBUG:print('addressbook:', len(addressbookData),line_number())
 
+	#keep newest copy of addressbook
 	addressbookData=addressbookData[-1].decode()
 
-	if DEBUG:print(addressbookData,line_number())
-
-	#parse data *[uName1,pubKey1@uname2,pubKey2]*
+	#parse entries from data *[uName1,pubKey1@uname2,pubKey2]*
 	if DEBUG:print('**check deliniating chars',line_number())
 	addressbookData=addressbookData.split('@')
 	for datum in addressbookData:
@@ -101,39 +102,31 @@ def load_addressbook(userName):
 
 		if DEBUG:print(datum,line_number())
 
-		if DEBUG:print(datum[1],len(datum[1]),line_number())
-
 		# add entries to addressbook
 		ADDRESSBOOK[datum[0]]=nacl.public.PublicKey(datum[1],encoder=nacl.encoding.HexEncoder)
 	#end load_addressbook
 
 def get_user_keys(keyString):
 	#begin get_user_keys
+	#get type key from string
 	keySet=nacl.public.PrivateKey(keyString)
 	return (keySet.public_key,keySet)
 	#end get_user_keys
 
 def setup_user():
 	#begin setup_user
-	global USER_KEYS,USER_NAME
+	global USER_KEYS,USER_NAME #set vars to global scope
 
 	#authenticate user func from Nathan, should return (keyBin,uname)
 	keyBin,userName=menu()
 
 	if not DEBUG:print('\033[H\033[J') #clear login screen
 
-	if DEBUG: print('**test code to delete',line_number())
-	'''
-	print('loading temp userFile',line_number())
-	file=open('userFile','r')
-	userData=file.readlines()
-	file.close()
-	'''
+	#set username and keys
 	USER_NAME=userName
 	USER_KEYS=get_user_keys(keyBin) #*(publKey,privKey)*
 
 	#load the users addressbook
-	# print('!!**load_addressbook disabled**!!',line_number())
 	load_addressbook(userName)
 	#end setup_user
 
@@ -161,7 +154,7 @@ def check_messages():
 
 	#query blockchain
 	messagesList=BLOCK.get_message_blockchain(convoID)
-	# if DEBUG:print('**',messagesList,line_number())
+
 	if DEBUG:print('**messages received:',len(messagesList),line_number())
 
 	#decrypt and display messages
@@ -170,13 +163,14 @@ def check_messages():
 		if not plainMessage=='':
 			print(plainMessage)
 
+	#pause to let user read messages
 	input('Press Enter To Continue: ')
 	#end check_messages
 
 def display_contacts():
 	#begin display_contacts	
 	global ADDRESSBOOK
-	#display contents of addressbook to std out
+	#print contents of addressbook to std out
 	count=1
 	print('\n Contacts')
 	for user in ADDRESSBOOK:
@@ -188,12 +182,15 @@ def display_contacts():
 def get_directory():
 	#begin get_directory
 	directoryDict={}
+
+	#get directory entries from blockchain and make dictionary
 	directoryList=BLOCK.get_message_blockchain(DIRECTORY_KEY)
 	for entry in directoryList:
 		key,value=entry.decode().split(',')
 		value=nacl.public.PublicKey(value,encoder=nacl.encoding.HexEncoder)
 		directoryDict[key]=value
 
+	#return dictionary of directory
 	return directoryDict
 	#end get_directory
 
@@ -216,31 +213,30 @@ def display_directory():
 
 def add_contact_from_card():
 	#begin add_contact_from_card
+	#get path to card from user
 	while True:
 		if DEBUG:print('need to sanitize',line_number())
 		filePath=input('Path to contact card: ')
-		#filePath='/home/chain/Desktop/bob.card'
+
+		#open contact card
 		try:
 			file=open(filePath,'r')
 		except IOError:
 			print('Bad file path')
 			continue
 		break
+
+	#get contact user name
 	splitPath=filePath.split('/')
-
 	contactName=splitPath[-1][:-5]
-
-	# if DEBUG:
-	# 	for x in range(len(splitPath)):
-	# 		print('**[%i] %s'%(x,splitPath[x]),line_number())
-
 	if DEBUG:print('**contactName:',contactName,line_number())
 
+	#get key from card file
 	hexPubKey=file.readline().rstrip()
 	file.close()
-
 	pubKey=nacl.public.PublicKey(hexPubKey,encoder=nacl.encoding.HexEncoder)
-	#if DEBUG:print('**',type(pubKey),line_number())
+
+	#add to addressbook
 	ADDRESSBOOK[contactName]=pubKey
 	#end add_contact_from_card
 
@@ -272,13 +268,12 @@ def add_contact_from_directory():
 def add_contact():
 	#begin add_contact
 	if DEBUG:print('**add_contact under construction',line_number())
-
 	if DEBUG:print('**add from directory option',line_number())
 
 	options=[]
 	validOptions=[]
 
-	#print add_contact menu
+	#print add_contact menu and build options list
 	if not DEBUG:print('\033[H\033[J')
 	print(' Contact Source')
 	print('\t1. Directory');validOptions.append(1);options.insert(1,add_contact_from_directory)
@@ -296,29 +291,32 @@ def add_contact():
 
 def remove_contact():
 	#begin remove_contact
-	if DEBUG:print('**remove_contact under construction',line_number())
 	global ADDRESSBOOK
+	if DEBUG:print('**remove_contact under construction',line_number())
+	
+	#buil dictionary of valid options and usernames
 	validDict={}
 	index=1
 	for key in ADDRESSBOOK:
 		validDict[index]=key
 		index+=1
+
+	#get choice from user
 	choice=get_valid_int([*validDict.keys()],'Selection: ')
 
+	#remove the user's choice
 	ADDRESSBOOK.pop(validDict[choice])
 	return False
 	#end remove_contact
 
 def update_addressbook():
 	#begn update_addressbook
-	if DEBUG:print('**update_addressbook under construction',line_number())
 	global USER_NAME
 	addressbookList=[]
 
-	# build string of addressbook to commit to blockchain
+	#build string of addressbook to commit to blockchain
 	for userName,pubKey in zip(ADDRESSBOOK.keys(),ADDRESSBOOK.values()):
 		addressbookList.append(userName+','+pubKey.encode(encoder=nacl.encoding.HexEncoder).decode())
-
 	addressbookString=ADDRESSBOOK_KEY_PREFIX+USER_NAME+'='+'@'.join(addressbookList)
 
 	if DEBUG:print(addressbookString) #debug
@@ -335,6 +333,7 @@ def create_contact_card():
 	#get filename and path to desktop
 	fileName=expanduser('~/Desktop/')+'%s.card'%USER_NAME
 
+	#open contact card
 	try:
 		file=open(fileName,'w')
 	except IOError:
@@ -349,12 +348,14 @@ def create_contact_card():
 	file.write(hexOfPubKey)
 	file.close()
 
-	print('Contact card creation successful')
+	#report success to user
+	print('Contact card saved to desktop')
 	sleep(2)
 	#end create_contact_card
 
 def save_changes():
 	#begin save_changes
+	#promt user if they want to save changes to addressbook
 	while True:
 		save=input('Save changes? [y/n]: ')
 		if save.lower()=='y' or save.lower()=='yes':
@@ -374,10 +375,11 @@ def edit_contacts():
 	while True:
 		if not DEBUG:print('\033[H\033[J')
 		global ADDRESSBOOK
-		#display contacts
+
+		#print contacts to std out
 		display_contacts()
 
-		#display edit contacts menu
+		#display edit contacts menu and build list of options
 		validOptions=[]
 		options=[]
 		print(' Edit contacts')
@@ -390,30 +392,8 @@ def edit_contacts():
 		#get user selection and validate
 		choice=get_valid_int(validOptions,'\n Selection: ')
 
-		if DEBUG:print('**remove after testing',line_number())
-		'''
-		while True:
-			try:
-				choice=int(input('\n Selection: '))
-			except ValueError:
-				print('Invalid Entry')
-				continue
-			if choice not in validOptions:print('Invalid Entry');continue
-			break
-		'''
-
 		#call chosen function
 		if options[choice]():break
-
-		if DEBUG: print('**delete after testing',line_number())
-		'''
-		if choice==0:
-			save_changes()
-		elif choice==1:
-			add_contact()
-		elif choice==2:
-			remove_contact()
-		'''
 	#end edit_contacts
 
 def main_menu():
@@ -422,6 +402,7 @@ def main_menu():
 	validOptions=[]
 	while True:
 		options=[]
+		#print main menu and build list of options
 		if not DEBUG and not SKIP_CLEAR:print('\033[H\033[J');SKIP_CLEAR=False
 		print(' Main Menu\n')
 		print('\t1. Check Messages');validOptions.append(1);options.insert(1,check_messages)
@@ -435,41 +416,12 @@ def main_menu():
 		#Get selection from user with validation
 		choice=get_valid_int(validOptions,' Selection: ')
 
-		if DEBUG:print('**delete after testing',line_number())
-		'''
-		while 1:
-			try:
-				choice=int(input(' Selection: '))
-			except ValueError:
-				print('Invalid Entry')
-				continue
-
-			if choice not in validOptions:print('Invalid Entry');continue
-			break
-		'''
-
 		#call function by choice
 		print('choice',choice,line_number())
 		if DEBUG:
 			for x in range(len(options)):
 				print('[%i]'%x,options[x])
 		options[choice]()
-
-		if DEBUG:print('**delete after testing',line_number())
-		
-		'''
-		if choice==0:			
-			graceful_exit()
-		elif choice==1:
-			check_messages()
-		elif choice==2:
-			send_message()
-		elif choice==3:
-			display_directory()
-			SKIP_CLEAR=True
-		elif choice==4:
-			edit_contacts()
-		'''
 	#end main_menu
 
 def greet_user():
@@ -500,7 +452,9 @@ def graceful_exit():
 
 def main():
 	#begin main
-	global DEBUG
+	global DEBUG #set var to global scope
+
+	#check for debug mode
 	if len(argv)==2:
 		if argv[1].lower()=='debug': DEBUG=True
 	if not DEBUG:print('\033[H\033[J')
